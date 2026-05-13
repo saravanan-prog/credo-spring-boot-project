b6946b162a105243a594466aea57534773e1b074
sarvanan-prog <saravanan.mca1992@gmail.com>
2026-05-13 18:27:55 +0530

    first commit

--- src/main/java/com/firstSpringproject/demoApp/DemoAppApplication.java
+++ src/main/java/com/firstSpringproject/demoApp/DemoAppApplication.java
@@ -4,20 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class DemoAppApplication implements CommandLineRunner  {
+public class DemoAppApplication implements CommandLineRunner {
 
 	@Autowired
 	private Employee e;
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoAppApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(DemoAppApplication.class, args);
+		Employee e = (Employee) context.getBean(Employee.class);
+		String data = e.toString();
+		System.out.println("Hi  My First spring boot project" + data);
+
 	}
 
-	public void run(String... args) throws  Exception{
-		System.out.println(e.toString());
-		System.out.println(e.getSalary());
-	}
+		public void run(String... args) throws  Exception{
+			System.out.println(e.toString());
+			System.out.println(e.getSalary());
+		}
 
 }
